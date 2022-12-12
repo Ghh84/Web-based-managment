aaf81329afb2a3023d1d8fc0476862c6fc5fbd29
Ghh84 <ghhiwethh@gmail.com>
2022-12-12 20:50:21 +0200

    add route component

--- src/src/App.js
+++ src/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Routes,Route,Switch } from 'react-router-dom';
+//import Home from './components/Home';
+import Transaction from './components/transactions/transaction';
+import Login from './components/login';
+import Logout from './components/logout';
+import PrivateRoute from './components/privateRoute'
+import PublicRoute from './components/publicRoute'
+import Users from './components/users/users'
+import Balance from './components/balance/balance'
+import Request from './components/Request/request';
+import AuthService from './services/auth.service';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <PrivateRoute component={Transaction} path='/Transactions' exact />
+          <PrivateRoute component={Users} path='/Users' exact />
+          <PrivateRoute component={Balance} path='/Balance' exact />
+          <PrivateRoute component={Request} path='/Request' exact />
+          <PublicRoute component={Login} path='/login' exact/>
+          <PublicRoute component={Login} path='/' exact/>
+          <PublicRoute component={Logout} path='/Logout' exact/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
