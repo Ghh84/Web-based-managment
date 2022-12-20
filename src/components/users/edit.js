7549ace6040a4e963f0d5c4e8bb4aa8389ab5937
Ghh84 <ghhiwethh@gmail.com>
2022-12-20 23:06:50 +0200

    Prepare Edit user component

---
+++ src/components/users/edit.js
@@ -0,0 +1,41 @@
+import { Button, Alert } from "react-bootstrap";
+const Edit=({  setPageState})=>{
+    return(
+        <div className="Page">
+            {/* <div className="section">
+                <div className="section-item">
+                    <b>Name</b>
+                    <input type='text'></input>
+                </div>
+                <div className="section-item">
+                    <b>Address</b>
+                    <input type='text'></input>
+                </div>
+                <div className="section-item">
+                    <b>Amount</b>
+                    <input type='text'></input>
+                </div>
+                <div className="section-item">
+                    <b>Currency</b>
+                    <input type='text'></input>
+                </div>
+                <div className="section-item">
+                    <b>reference</b>
+                    <input type='text'></input>
+                </div>
+            </div>
+            <div className="list-buttons">
+            <Button
+                    variant="success"
+                    onClick={() => setPageState({ isEdit: 0, isAdd: 0 })}
+                  >Save</Button>
+            <Button
+                    variant="warning"
+                    onClick={() => setPageState({ isEdit: 0, isAdd: 0 })}
+                  >Cancel</Button>
+            </div> */}
+        </div>
+    )
+    }
+    
+    export default Edit
