737dee0c62ffc37b63028d29ed07a456f817bd27
Ghh84 <ghiwethh@gmail.com>
2022-12-25 18:31:50 +0200

    Add update controller function

--- api/controllers/balance.controller.js
+++ api/controllers/balance.controller.js
@@ -16,6 +16,46 @@ const getBalances = async (req, res) => {
   }
 })
 };
+const getBalance = async (req,res) =>{
+  console.log("came in balance controller...........");
+  const values=[]
+  for (const [key, value] of Object.entries(req.body)) {
+    console.log(`${key}: ${value}`);
+    values.push(value)
+  }
+  console.log( values)
+  connection.query("SELECT USDbalance, localBalance from  balance where userId=?",[values[0]],(err,row)=>{
+  if(err){
+    console.log('error occured',err)
+  }
+  else{
+    console.log('returned records')
+    res.status(200).send(row)
+  }
+  
+})
+}
+const updateBalance = (req, res) => {
+  let columns=[],values=[]
+  for (const [key, value] of Object.entries(req.body)) {
+    console.log(`${key}: ${value}`);
+    values.push(value)
+  }
+  console.log('values',values)
+  connection.query("UPDATE  balance SET USDbalance=?,localBalance=?,adminUSD=?,adminLocal=?,comment=? where userId=?",
+  [values[1],values[2],values[3],values[4],values[5],values[0]],(err,row)=>{
+      if(err){
+        console.log('error adding to transactions',err)
+        res.status(400).send('Error updating the transaction')
+      }
+      else{
+        console.log('returned records',row)
+        res.status(200).send(row)
+      }
+    }) 
+  
+  
+};
 
 module.exports= {
   updateBalance,
