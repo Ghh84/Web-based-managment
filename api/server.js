418a96570f28bcf58051b2507b7856a42c172766
Ghh84 <ghiwethh@gmail.com>
2022-12-27 15:43:36 +0200

    Revised and delete unneccessary code

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port=process.env.PORT || 8080;
 const app=express()
 const bodyParser = require("body-parser");
+
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,39 +13,9 @@ require("./routes/transaction.route")(app);
 require("./routes/user.route")(app);
 require("./routes/balanceRequest.route")(app);
  require("./routes/balance.route")(app);
-/*require("./routes/transaction.route")(app);
-require("./routes/user.route")(app); */
-/*var connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'password',
-    database:'demoDb',
-    port:'3306'
+require("./routes/transaction.route")(app);
+require("./routes/user.route")(app); 
 
-})
-connection.connect((err)=>{
-    if(err){
-        throw err
-    }else {
-        console.log('connected!')
-    }
-})
-connection.query("INSERT INTO transactions(name,country,amount,reference,agent_id) VALUES ('Amanuel','canada','100','Elsa','1')",(err,rows)=>{
-    if(err){
-        throw err
-    }else {
-        console.log('data sent...')
-        console.log(rows)
-    }
-}) */
-/* connection.query("INSERT INTO agent(name,username,password,role) VALUES ('Amanuel','aloniab','aloni','1')",(err,rows)=>{
-    if(err){
-        throw err
-    }else {
-        console.log('data sent...')
-        console.log(rows)
-    }
-}) */
 
 app.listen(port)
 console.log('App listening on port '+port)
